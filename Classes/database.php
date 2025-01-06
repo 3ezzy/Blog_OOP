d4aac80e4c6434726f8b7c4396bb21f510093e82
Mehdi <khammali26@gmail.com>
2025-01-06 23:14:01 +0100

    Add Database class for PDO connection management

---
+++ Classes/database.php
@@ -0,0 +1,34 @@
+<?php 
+
+    class Database {
+        private $dsn = "mysql:host=localhost;dbname=gestionBlog";
+        private $username = "root";
+        private $password = "123";
+        public $conn;
+    
+        // Constructor to establish a database connection using PDO
+        public function __construct() {
+            try {
+                $this->conn = new PDO($this->dsn, $this->username, $this->password);
+                // Set the PDO error mode to exception
+                $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            } catch (PDOException $e) {
+                die("Connection failed: " . $e->getMessage());
+            }
+        }
+    
+        public function query($sql) {
+            return $this->conn->query($sql);
+        }
+    
+        
+        public function prepare($sql) {
+            return $this->conn->prepare($sql);
+        }
+    
+        
+        public function close() {
+            $this->conn = null;
+        }
+    }
+?>
